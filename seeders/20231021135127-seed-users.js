2ab2b6ce91ba5520b2eb8b8198cedfbebe73248e
darshanaslp <darshanaslp@hotmail.com>
2023-10-24 03:10:24 +0530

    user seeders

--- seeders/20231021135127-seed-users.js
+++ seeders/20231021135127-seed-users.js
@@ -6,14 +6,16 @@ const bcrypt = require('bcrypt');
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const hashedPassword1 = await bcrypt.hash('admin_password', 10); // Change 'admin_password' to the desired password
-    const hashedPassword2 = await bcrypt.hash('customer_password', 10); // Change 'customer_password' to the desired password
+    const adminPassword = await bcrypt.hash('admin_password', 10); // Change 'admin_password' to the desired password
+    const customerPassword = await bcrypt.hash('customer_password', 10); // Change 'customer_password' to the desired password
+	const shopAdminPassword = await bcrypt.hash('shopadmin_password', 10); // Change 'shopadmin_password' to the desired password
+	const systemAdminPassword = await bcrypt.hash('systemadmin_password', 10); // Change 'systemadmin_password' to the desired password
 
     return queryInterface.bulkInsert('users', [
       {
         username: 'admin',
         email: 'admin@example.com',
-        password: hashedPassword1,
+        password: adminPassword,
         role: 'admin',
         createdAt: new Date(),
         updatedAt: new Date(),
@@ -21,7 +23,7 @@ module.exports = {
       {
         username: 'admin1',
         email: 'admin1@example.com',
-        password: hashedPassword1,
+        password: shopAdminPassword,
         role: 'shopAdmin', // Use 'shopAdmin' to match the model definition
         createdAt: new Date(),
         updatedAt: new Date(),
@@ -29,7 +31,7 @@ module.exports = {
       {
         username: 'customer1',
         email: 'customer1@example.com',
-        password: hashedPassword2,
+        password: customerPassword,
         role: 'customer',
         createdAt: new Date(),
         updatedAt: new Date(),
@@ -37,12 +39,11 @@ module.exports = {
       {
         username: 'admin2',
         email: 'admin2@example.com',
-        password: hashedPassword1, // Use the same hashed password
+        password: systemAdminPassword, // Use the same hashed password
         role: 'systemAdmin', // Add 'systemAdmin'
         createdAt: new Date(),
         updatedAt: new Date(),
       },
-      // Add more user data as needed
     ]);
   },
 
